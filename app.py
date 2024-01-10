d8bce946ca01ca0f5c0484e7d388564b077c6598
Muhammad Iman Zulhakim B. Mohd Zulkhori 288430 <imanzulhakim0831@gmail.com>
2024-01-11 00:36:52 +0800

    commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/iman")
+def iman():
+    return render_template('iman.html')
+
+
+app.run()
+
+
