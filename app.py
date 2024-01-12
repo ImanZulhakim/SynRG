125917dcd54f81f715c8b99e7c33e233d7515805
Tahfiz <tahfizzudeenk6md16@gmail.com>
2024-01-13 00:00:08 +0800

    designing and add content for portfolio.html

--- app.py
+++ app.py
@@ -3,22 +3,17 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 # run_with_ngrok(app)
-
-
 @app.route("/")
 def home():
     return render_template('index.html')
-
-
 @app.route("/iman")
 def iman():
     return render_template('iman.html')
-
 @app.route("/hobby")
 def hobby():
     return render_template('hobby.html')
 
-app.route("/portfolio")
+@app.route("/portfolio")
 def portfolio():
     return render_template('portfolio.html')
 
