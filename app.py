0e61541699f3f962f193c67038a9eb8213908082
Tahfiz <tahfizzudeenk6md16@gmail.com>
2024-01-12 17:12:46 +0800

    Add 'portfolio.html' page with content of Iman Zulhakim's information.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
-from flask_ngrok import run_with_ngrok
+# from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)
+# run_with_ngrok(app)
 
 
 @app.route("/")
