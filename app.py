1567501bb44a39cf157444edd4ebc66e653bc45f
Muhammad Iman Zulhakim B. Mohd Zulkhori 288430 <imanzulhakim0831@gmail.com>
2024-01-13 14:40:40 +0800

    solved the typed in landing page and navbar scroll

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
 from flask import Flask, render_template, request, redirect
+
 # from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+
+
 # run_with_ngrok(app)
 @app.route("/")
 def home():
     return render_template('index.html')
-@app.route("/iman")
-def iman():
-    return render_template('iman.html')
+
+
+@app.route("/main")
+def main():
+    return render_template('main.html')
+
+
 @app.route("/hobby")
 def hobby():
     return render_template('hobby.html')
+
 
 @app.route("/portfolio")
 def portfolio():
     return render_template('portfolio.html')
 
+
 @app.route("/help")
 def help():
     return render_template('help.html')
+
 
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
 
+
 app.run()
-
-
